5aa8781600290a2f4a4f677087915b4a1103b8dc
nilay <nilay_994@hotmail.com>
2020-03-18 15:01:39 +0100

    [whydoidothis] superhacky vel obs

--- velobs/velobs2.py
+++ velobs/velobs2.py
@@ -23,6 +23,11 @@ def config_matplotlib():
     plt.rcParams['xtick.color'] = gray
     plt.rcParams['ytick.color'] = gray
 
+def cart2polar(vel_vec):
+    mag = np.linalg.norm(vel_vec)
+    direction = np.arctan2(vel_vec[1], vel_vec[0])
+    return mag, direction
+
 # dir = angle in radians wrt +x
 def polar2cart(mag, direction):
     x = mag * np.cos(direction)
@@ -35,6 +40,9 @@ class robot:
         self.pos = pos
         self.vel = vel
         self.head = head
+        # create copies for after conflict resolution
+        self.oldvel = vel
+        self.oldhead = head
 
     def move(self):
         [delx, dely] = polar2cart(self.vel*0.2, self.head)
@@ -61,13 +69,13 @@ def pos_vel_mat(robot_obj_list):
     velmat = np.zeros((len(robot_obj_list), 2))
 
     for i in range(len(robot_obj_list)):
-        posmat[i,:] = [robot_obj_list[i].pos[0], robot_obj_list[i].pos[1]]
+        posmat[i,:] = np.array([robot_obj_list[i].pos[0], robot_obj_list[i].pos[1]])
         velmat[i,:] = polar2cart(robot_obj_list[i].vel, robot_obj_list[i].head)
 
     return posmat, velmat
 
 # break isolation, share amongst the instances :p
-def detect(robot_obj_list):
+def detect2(robot_obj_list):
     num_robots = len(robot_obj_list)
 
     # build matrix 
@@ -78,24 +86,28 @@ def detect(robot_obj_list):
     tcpa = np.zeros((num_robots, num_robots))
     dcpa = np.zeros((num_robots, num_robots))
 
-    for i in range(num_robots):
-        relmat = rel_mat(i, num_robots)
-        # relative vel and pos with each robot (3 x 2)
-        drel = relmat * posmat
-        vrel = relmat * velmat
-        # print (i, drel, vrel)
-        
-        for j in range(num_robots):
-            if j!=i:
-                norm_vrel = np.linalg.norm(vrel[j,:])
-                if norm_vrel < 0.1:
-                    norm_vrel = 0.1
-                tcpa[i,j] = - np.inner(drel[j,:], vrel[j,:]) / (norm_vrel**2)
-                dcpa[i,j] = (abs((np.linalg.norm(drel[j,:])**2) - ((tcpa[i,j]**2) * (norm_vrel**2)))) ** (1./2)
-                # print (i,j)
+    i = 0
+    relmat = rel_mat(i, num_robots)
+    # relative vel and pos with each robot (3 x 2)
+    drel = relmat * posmat
+    vrel = relmat * velmat
+    # print ('-------------------------')
+    # print (i)
+    # print (drel)
+    # print (vrel)
+    # print ('-------------------------')
+    
+    for j in range(num_robots):
+        if j!=i:
+            norm_vrel = np.linalg.norm(vrel[j,:])
+            if norm_vrel < 0.1:
+                norm_vrel = 0.1
+            tcpa[i,j] = - np.inner(drel[j,:], vrel[j,:]) / (norm_vrel**2)
+            dcpa[i,j] = (abs((np.linalg.norm(drel[j,:])**2) - ((tcpa[i,j]**2) * (np.linalg.norm(vrel[j,:])**2)))) ** (1./2)
+
     print('----------------------')
-    print(tcpa)
-    print(dcpa)
+    print(np.round(tcpa,2))
+    print(np.round(dcpa,2))
     print('----------------------')
     # n x n symmetric matrix is now populated 
     
@@ -103,15 +115,75 @@ def detect(robot_obj_list):
         
 # def resolve(robot_obj_list, occupancy_map):
 
-# def detect(robot_a, robot_b):
-#     drel = robot_a.pos - robot_b.pos
-#     vrel = polar2cart(robot_a.vel, robot_a.head) - polar2cart(robot_b.vel, robot_b.head)
-#     norm_vrel = np.linalg.norm(vrel)
-#     if norm_vrel < 0.1:
-#         norm_vrel = 0.1
-#     tcpa = - np.inner(drel, vrel) / (norm_vrel**2)
-#     dcpa = (abs((np.linalg.norm(drel)**2) - ((tcpa ** 2) * (np.linalg.norm(vrel)**2)))) ** (1./2)
-#     print(round(tcpa, 5), round(dcpa,5))
+# hacky direct version
+def detect(robot_a, robot_b):
+    drel = robot_a.pos - robot_b.pos
+    vrel = polar2cart(robot_a.vel, robot_a.head) - polar2cart(robot_b.vel, robot_b.head)
+    norm_vrel = np.linalg.norm(vrel)
+    if norm_vrel < 0.1:
+        norm_vrel = 0.1
+    tcpa = - np.inner(drel, vrel) / (norm_vrel**2)
+    dcpa = (abs((np.linalg.norm(drel)**2) - ((tcpa ** 2) * (np.linalg.norm(vrel)**2)))) ** (1./2)
+    if (tcpa > 0) and (dcpa < 0.5):
+        newvela = resolve(robot_a, robot_b)
+        robot_a.vel, robot_a.head = cart2polar(newvela)
+        print (robot_a.vel, np.rad2deg(robot_a.head))
+    else: 
+        robot_a.vel = robot_a.oldvel
+        robot_a.head = robot_a.oldhead
+#     print(round(tcpa, 2), round(dcpa, 2))
+
+
+def project(robot_a, angle1, angle2, centre):
+    
+    vela = polar2cart(robot_a.vel, robot_a.head)
+
+    yintercept = centre[1] - angle1 * centre[0]
+    # projection is the x-coordinate itself
+    if angle1 == 0:
+        newvela = np.array([va[0], 0])
+        return newvela
+    else:
+        xintercept = yintercept / angle1
+
+    # slope point to slope intercept
+    # x_intercept = 3, y intercept = 1.5, slope = -0.5
+    # line_prop = [3, 1.5, -0.5]
+    line_prop = [xintercept, yintercept, angle1]
+    
+    # slope - intercept form, poly1d(m, c)
+    line1 = np.poly1d([line_prop[2], line_prop[1]])
+
+    # calculate vector passing through origin, y = mx
+    vector_from_line = np.array([line_prop[0], -line_prop[1]])
+    if (np.inner(vector_from_line, vector_from_line) > 0.1):
+        # calculate projection matrix
+        P = np.outer(vector_from_line, vector_from_line)/np.inner(vector_from_line, vector_from_line)
+    else: 
+        newvela = vela
+        return newvela
+    # print(P)
+    # projection matrix P**2 = P
+    # print(P.dot(P))
+    
+    projected_pt = P.dot(vela + robot_a.pos - np.array([0, line_prop[1]]))
+    newvela = projected_pt
+
+    return newvela
+
+def resolve(robot_a, robot_b):
+    # build solution space diagram
+    # find angle of b in world frame
+    angleb = np.arctan2(robot_b.pos[1], robot_b.pos[0])
+    # do atan dist here
+    deltad = np.linalg.norm(robot_a.pos - robot_b.pos)
+    angleb1 = angleb + np.arctan(RR/deltad)
+    angleb2 = angleb - np.arctan(RR/deltad)
+    # centre of collision cone
+    centre = robot_a.pos + robot_b.pos
+    
+    newvela = project(robot_a, angleb1, angleb2, centre)
+    return newvela
 
 robot_obj_list = []
 def main():
@@ -125,15 +197,16 @@ def main():
     plt.xlim(-30, 30)
     plt.ylim(-30, 30)
 
-    robot_a = robot(np.array([10.0, -10.0]), 2, np.deg2rad(135))
-    robot_b = robot(np.array([-12.0, 8.0]), 2, np.deg2rad(-45))
+    robot_a = robot(np.array([7.0, 0.0]), 2, np.deg2rad(135))
+    robot_b = robot(np.array([-7.0, 0.0]), 2, np.deg2rad(45))
     
     robot_obj_list.append(robot_a)
     robot_obj_list.append(robot_b)
 
-    for i in range(20):
+    for i in range(80):
         # plt.cla()
-        detect(robot_obj_list)
+        detect(robot_a, robot_b)
+        # detect2(robot_obj_list)
         plt.plot(block = 'False')
         robot_a.move()
         robot_b.move()
