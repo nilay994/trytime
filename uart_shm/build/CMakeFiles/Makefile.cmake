169049bdb7f2abc9072604d1c0804328b3e715ad
fede <fde014@gmail.com>
2020-07-22 17:15:30 +0200

    changed filename

--- uart_shm/build/CMakeFiles/Makefile.cmake
+++ uart_shm/build/CMakeFiles/Makefile.cmake
@@ -49,5 +49,5 @@ set(CMAKE_MAKEFILE_PRODUCTS
 
 # Dependency information for all targets:
 set(CMAKE_DEPEND_INFO_FILES
-  "CMakeFiles/uart_serial_example.dir/DependInfo.cmake"
+  "CMakeFiles/uart_shm_example.dir/DependInfo.cmake"
   )
---
+++ uart_shm/build/CMakeFiles/uart_shm_example.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/uart_shm_example.dir/src/uart_main.cpp.o"
+  "uart_shm_example.pdb"
+  "uart_shm_example"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/uart_shm_example.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
