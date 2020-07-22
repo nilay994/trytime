df65d4b5074404b0ac02c7183c2d44539851ccf6
fede <fde014@gmail.com>
2020-07-22 17:17:38 +0200

    cleaning

--- uart_struct/include/crcfast.h
+++
@@ -1,7 +0,0 @@
-// defines for CRC calcuation
-#define WIDTH  (8 * sizeof(uint8_t))
-#define TOPBIT (1 << (WIDTH - 1))
-#define POLYNOMIAL 0xD8  /* 11011 followed by 0's */
-
-// CRC lookup table
-uint8_t  crcTable[256];
